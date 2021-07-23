4d4da24a8cffa7f82445cd4a4cab6564f7f07c37
david-p-burton <david.p.burton@gmail.com>
2021-07-23 23:43:55 +0100

    Finished tutorial

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+const Hello = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old!</p>
     </div>
-  );
+  )
 }
 
-export default App;
+const Footer = () => {
+  return(
+    <div>
+      I am learning!
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+      <Footer/>
+    </>
+  )
+}
+
+export default App
